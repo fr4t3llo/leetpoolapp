5f4a3c0a22ab0e01f60a5229ee9906c97c4d6ac1
FRATELLO <saife.addine123@gmail.com>
2025-05-22 23:50:56 +0100

    update by FRATELLO

--- lib/api/auth.dart
+++ lib/api/auth.dart
@@ -45,7 +45,7 @@ class _AuthPageState extends State<AuthPage> {
                       ],
                     ),
                   ),
-                  
+
                 ),
               ],
             ),
